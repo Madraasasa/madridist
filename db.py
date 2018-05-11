c2e3cc6210cb978a4758eedc30cabc62299aa4c4
Maruf <36636180+Madraasasa@users.noreply.github.com>
2018-05-11 17:12:17 +0500

    Add files via upload

--- db.py
+++ db.py
@@ -5,11 +5,15 @@ import datetime
 import timetable
 import timetable1
 import requests, bs4
+
 bot = telebot.TeleBot(config.token)
-con = p.connect(database='dfq6banncblc4l', user='vtudqaibjctcaw', host='ec2-23-23-142-5.compute-1.amazonaws.com', password='d9dcd45b804a1510e232ff7b13f9ff260332692b9328bef3e21b8a752ffb389c')
-cur=con.cursor()
-a=[]
-def start(message,fuc,kurs):
+con = p.connect(database='dfq6banncblc4l', user='vtudqaibjctcaw', host='ec2-23-23-142-5.compute-1.amazonaws.com',
+                password='d9dcd45b804a1510e232ff7b13f9ff260332692b9328bef3e21b8a752ffb389c')
+cur = con.cursor()
+a = []
+
+
+def start(message, fuc, kurs):
     cur.execute("select * from users")
     a.clear()
     for row in cur:
@@ -21,21 +25,35 @@ def start(message,fuc,kurs):
 
         a.clear()
     else:
-        cur.execute('INSERT INTO users VALUES ('+str(message.from_user.id)+','+"'"+fuc+"'"+','+str(kurs)+');')
+        cur.execute(
+            'INSERT INTO users VALUES (' + str(message.from_user.id) + ',' + "'" + fuc + "'" + ',' + str(kurs) + ');')
 
         con.commit()
         a.clear()
         return "Упешно добавлена"
+
+
 def rassilka():
     cur.execute("select * from users")
     dn = datetime.date.today().isoweekday()
+    if dn == 7:
+        dn = 1
+        config.file += 1
+        a = str(config.file) + '.xlsx'
+        timetable.start(a)
+        config.week += 1
+        a = str(config.week) + '.xlsx'
+        timetable1.start(a)
+    else:
+        dn=dn+1
+
     m = datetime.date.today().month
     if (len(str(m))) == 1:
         m = '0' + str(m)
     d = datetime.date.today().day + 1
     print(d)
     s = requests.get('https://sinoptik.com.ru/погода-душанбе/2018-' + m + '-' + str(d))
-    privet='Удачной учебы @MGURASP_Bot'
+    privet = 'Удачной учебы @MGURASP_Bot'
     b = bs4.BeautifulSoup(s.text, "html.parser")
     p3 = b.select('.temperature .p3')
     pogoda1 = p3[0].getText()
@@ -50,53 +68,53 @@ def rassilka():
     p = b.select('.rSide .description')
     pogoda = p[0].getText()
     c = pogoda.strip()
-    privet1="Добрый вечер!!! "+'\n' +'Рассписание на завтра:'
+    privet1 = "Добрый вечер!!! " + '\n' + 'Рассписание на завтра:'
     for row in cur:
         print(row[1])
         print(row[2])
-        if row[1] in ("МО", "ГМУ","Лингвистика", "Реклама"):
+        if row[1] in ("МО", "ГМУ", "Лингвистика", "Реклама"):
             if row[1] == 'МО':
                 z = timetable1.get_day("МО", int(row[2]), dn)
-                k=z.rindex('\n',0, 30)
-                z=z[k+1:]
+                k = z.rindex('\n', 0, 30)
+                z = z[k + 1:]
                 z = z + '\n' + x + '\n' + y + '\n' + c + '\n' + privet
             elif row[1] == 'ГМУ':
                 z = timetable1.get_day("ГМУ", row[2], dn)
-                k=z.rindex('\n',0, 30)
-                z=z[k+1:]
-                z = z + '\n' + x + '\n' + y + '\n' + c+'\n'
+                k = z.rindex('\n', 0, 30)
+                z = z[k + 1:]
+                z = z + '\n' + x + '\n' + y + '\n' + c + '\n'
             elif row[1] == "Лингвистика":
                 z = timetable1.get_day("ЛИНГВ", row[2], dn)
-                k=z.rindex('\n',0, 30)
-                z=z[k+1:]
+                k = z.rindex('\n', 0, 30)
+                z = z[k + 1:]
                 z = z + '\n' + x + '\n' + y + '\n' + c + '\n'
             elif row[1] == "Реклама":
                 z = timetable1.get_day("РЕКС", row[2], dn)
-                k=z.rindex('\n',0, 30)
-                z=z[k+1:]
-                z = z + '\n' + x + '\n' + y + '\n' + c + '\n'  
-            z=privet1+'\n'+z+'\n'+privet
+                k = z.rindex('\n', 0, 30)
+                z = z[k + 1:]
+                z = z + '\n' + x + '\n' + y + '\n' + c + '\n'
+            z = privet1 + '\n' + z + '\n' + privet
             z = z.replace("*", "")
 
             bot.send_message(row[0], z)
-        elif row[1] in ("ПМИ", "Геология","Химия"):
+        elif row[1] in ("ПМИ", "Геология", "Химия"):
             if row[1] == 'ПМИ':
-                z = timetable.get_day("ПМИИ",int(row[2]), dn)
-                k=z.rindex('\n',0, 30)
-                z=z[k+1:]
+                z = timetable.get_day("ПМИИ", int(row[2]), dn)
+                k = z.rindex('\n', 0, 30)
+                z = z[k + 1:]
                 z = z + '\n' + x + '\n' + y + '\n' + c + '\n'
             elif row[1] == 'Геология':
 
                 z = timetable.get_day("ГЕОЛ", row[2], dn)
-                k=z.rindex('\n',0, 30)
-                z=z[k+1:]
+                k = z.rindex('\n', 0, 30)
+                z = z[k + 1:]
                 z = z + '\n' + x + '\n' + y + '\n' + c + '\n'
                 print(z)
             elif row[1] == 'Химия':
                 z = timetable.get_day("ХФММ", row[2], dn)
-                k=z.rindex('\n',0, 30)
-                z=z[k+1:]
+                k = z.rindex('\n', 0, 30)
+                z = z[k + 1:]
                 z = z + '\n' + x + '\n' + y + '\n' + c + '\n'
             z = z.replace("*", "")
-            z = privet1+'\n'+z + '\n' + privet
+            z = privet1 + '\n' + z + '\n' + privet
             bot.send_message(row[0], z)
